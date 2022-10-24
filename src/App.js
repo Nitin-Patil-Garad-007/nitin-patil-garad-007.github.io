05c75fdf325f22aa5042d981453376296ea6beb1
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1~ <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1~>
2022-10-24 18:14:59 +0530

    fix refresh problem in github pages.

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ import MovieHomePage from './Pages/MovieHomePage'
 function App() {
   return (
   <>
-    <Routes>
+    <Routes basename="react_movies">
     <Route path='/' element={<Details />} />
       <Route path='/registration' element={<RegistrationPage />} />
       <Route path='/login' element={<Login />} />
